766f43e8bebde4b17d8c505177a7ccd7e1c8013b
solpanozo <63618163+solpanozo@users.noreply.github.com>
2020-05-21 11:18:38 -0300

    Add files via upload

---
+++ Adicion.cpp
@@ -0,0 +1,19 @@
+/* Hello World
+Sol Panozo
+202004
+*/
+
+#include <iostream>
+
+int main(){
+    std::cout << "Inserte valor a: ";
+    int a;
+    std::cin >> a;
+    int b;
+    std::cout << "Inserte valor b: ";
+    std::cin >> b;
+    int c;
+    c = a + b;
+    std::cout << "El resultado es: " << c <<std::endl;
+    return 0;
+}
